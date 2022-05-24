e289ec70191bab8020a778da1cb98c4e04051ae5
Documenter.jl <documenter@juliadocs.github.io>
2022-05-24 20:41:18 +0000

    build based on 11fe32f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PHYS5315","category":"page"},{"location":"#PHYS5315","page":"Home","title":"PHYS5315","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PHYS5315.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PHYS5315]","category":"page"}]
+}
